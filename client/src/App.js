014ef40a21a300c06160f38aa10fb58351e06ca0
f1reboywatergrl <vincentius.samuelgk2@gmail.com>
2021-01-28 09:49:22 +0700

    task complete (no additional features yet)

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,24 @@ import './App.css';
 import TodoList from './TodoList';
 
 function App() {
-  const tasks = useState([
+  const [tasks,setTasks] = useState([
     "Cuci Baju",
     "Masak Nasi"
   ]);
+  const [input, setInput] = useState('');
+  const handleChange = (tBox) => {
+    setInput(tBox.target.value);
+  }
+  const handleSubmit = () => {
+    setTasks([...tasks,input]);
+    setInput('');
+  }
   return (
     <div className="App">
       <h1>Pekerjaan Rumah Yang Perlu Dilakukan</h1>
-      <input value=""/>
-      <button onClick="">Tambah</button>
-      <TodoList tasks={tasks} />
+      <input value={input} onChange = {handleChange} />
+      <button style={{marginLeft:'0.5%'}} onClick={handleSubmit}>Tambah</button>
+      <TodoList tasks={tasks} onTasksChange={setTasks} />
     </div>
   );
 }
--- client/src/TodoList.jsx
+++ client/src/TodoList.jsx
@@ -1,10 +1,32 @@
-import React from "react";
+import React,{useCallback} from "react";
 
-export default function TodoList() {
+export default function TodoList({tasks,onTasksChange}) {
+
+  const handleDelete = useCallback((list,index) => {
+    onTasksChange(list.slice(0,index).concat(list.slice(index+1,list.length)))
+    // useForceUpdate();
+  },[onTasksChange])
+
+  const displayList = (list) => {
+    let listComponent = [];
+    for (let i=0;i<list.length;i++){
+      listComponent.push(
+        <li key={i}>
+          {list[i]}
+          <button style={{marginLeft:'2%'}} onClick=
+          {()=> handleDelete(list,i)}>
+          Hapus</button>
+        </li>
+      )
+    }
+    return listComponent;
+  }
   return (
-    <div>
-      <li>Cuci Baju <button>Hapus</button></li>
-      <li>Masak Nasi <button>Hapus</button></li>
+    <div style={{display:'flex',justifyContent:'center'}}>
+      <ul style={{width:'15%'}}>
+        {displayList(tasks)}
+      </ul>      
     </div>
+
   );
 }
